b2e713139f5a6e63ddf8db0beaf0178ed0d83535
Bakr Hawsawi <122125253+Bakr40@users.noreply.github.com>
2023-05-27 19:15:48 +0300

    commit

--- TourismProject/TourismProject/settings.py
+++ TourismProject/TourismProject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "tourism"
+    "tourism",
+    "bootstrap5",
 ]
 
 MIDDLEWARE = [
