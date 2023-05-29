f2c9a93a735b59eef0d4381eddf6bf73f7683e7f
lena <lenaashayban@gmail.com>
2023-05-29 12:58:58 +0300

    project

--- Project/app/models.py
+++ Project/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Cities(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    rating = models.FloatField()
+
